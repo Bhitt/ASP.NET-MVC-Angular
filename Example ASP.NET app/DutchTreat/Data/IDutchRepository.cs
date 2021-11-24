9b09b6a5918ea978bafd7b05bead5df57c86ce64
brandEn <BrandenHitt@gmail.com>
2021-11-24 13:25:53 -0800

    Initial Commit
    Add existing code (from sections 1 through 10)

---
+++ Example ASP.NET app/DutchTreat/Data/IDutchRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DutchTreat.Data.Entities;
+
+namespace DutchTreat.Data
+{
+    public interface IDutchRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetProductsByCategory(string category);
+        IEnumerable<Order> GetAllOrders(bool includeItems);
+        Order GetOrderById(string username, int id);
+        public bool SaveAll();
+        void AddEntity(object model);
+        object GetAllOrdersByUser(string username, bool includeItems);
+    }
+}
